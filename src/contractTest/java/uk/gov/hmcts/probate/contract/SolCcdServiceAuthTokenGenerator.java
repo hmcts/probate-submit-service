513f07c5a94e8aa3212d9123f5540170d6466001
Dhruv Patel <dhruv.patel20@gmail.com>
2018-08-14 12:50:12 +0100

    Fixed test for SIDAM

--- src/contractTest/java/uk/gov/hmcts/probate/contract/SolCcdServiceAuthTokenGenerator.java
+++ src/contractTest/java/uk/gov/hmcts/probate/contract/SolCcdServiceAuthTokenGenerator.java
@@ -59,8 +59,8 @@ public class SolCcdServiceAuthTokenGenerator {
         return tokenGenerator.generate();
     }
 
-    public int getUserId() {
-        int userid_local = RestAssured.given()
+    public String getUserId() {
+        String userid_local = "" + RestAssured.given()
                 .header("Authorization", userToken)
                 .get(idamUserBaseUrl + "/details")
                 .body()
