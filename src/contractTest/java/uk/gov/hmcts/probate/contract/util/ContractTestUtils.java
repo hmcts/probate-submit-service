5ca822f1a5ef5ad598c50aa7ab63b87ae65ff2e0
Dhruv Patel <dhruv.patel20@gmail.com>
2018-08-29 15:29:09 +0100

    PRO-2344: Updated contract tests.

--- src/contractTest/java/uk/gov/hmcts/probate/contract/util/ContractTestUtils.java
+++ src/contractTest/java/uk/gov/hmcts/probate/contract/util/ContractTestUtils.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.probate.contract.util;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
@@ -7,13 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.util.ResourceUtils;
-import uk.gov.hmcts.probate.contract.TestContextConfiguration;
 import uk.gov.hmcts.probate.contract.SolCcdServiceAuthTokenGenerator;
+import uk.gov.hmcts.probate.contract.TestContextConfiguration;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 
 @ContextConfiguration(classes = TestContextConfiguration.class)
@@ -23,22 +24,19 @@ public class ContractTestUtils {
     @Autowired
     protected SolCcdServiceAuthTokenGenerator solCcdServiceAuthTokenGenerator;
 
-    private String serviceToken;
+    private ObjectMapper objectMapper;
 
+    private String serviceToken;
 
     @PostConstruct
     public void init() {
         serviceToken = solCcdServiceAuthTokenGenerator.generateServiceToken();
+        objectMapper = new ObjectMapper();
     }
 
-    public String getJsonFromFile(String fileName) {
-        try {
-            File file = ResourceUtils.getFile(this.getClass().getResource("/json/" + fileName));
-            return new String(Files.readAllBytes(file.toPath()));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public JsonNode getJsonNodeFromFile(String fileName) throws IOException {
+        File file = ResourceUtils.getFile(this.getClass().getResource("/json/" + fileName));
+        return objectMapper.readTree(file);
     }
 
     public Headers getHeaders() {
@@ -67,7 +65,4 @@ public class ContractTestUtils {
     public String getUserId() {
         return solCcdServiceAuthTokenGenerator.getUserId();
     }
-
-
-
 }
