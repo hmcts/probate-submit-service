743c271e3f8bd9d4e7da041f0fa7f772015af165
sparekh <sanjay.parekh@hmcts.net>
2018-09-28 14:46:13 +0100

    PRO-3501: add ccd mapping change

--- src/test/java/uk/gov/hmcts/probate/services/submit/clients/CoreCaseDataMapperTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/submit/clients/CoreCaseDataMapperTest.java
@@ -101,7 +101,6 @@ public class CoreCaseDataMapperTest {
     public void mapDataTest() {
         JsonNode mappedData = coreCaseDataMapper.mapData(submitdata, submissonTimestamp, registryData);
         JsonNode registry = registryData.get("registry");
-        assertTrue(mappedData.get("applicationSubmittedDate").asText().equals("2017-08-24"));
         assertTrue(mappedData.get("applicationID").equals(registryData.get("submissionReference")));
         assertTrue(mappedData.get("registryLocation").equals(registry.get("name")));
         assertNotNull(mappedData.get("primaryApplicantForenames"));
