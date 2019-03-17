3574684a8ff0e63019b299fd5ca82dac88a077bc
Jake <jake.huggins@solirius.com>
2019-03-17 18:28:41 +0000

    PRO-4949: refactor CcdClientApiErrorDecoder

--- src/main/java/uk/gov/hmcts/probate/services/submit/clients/v2/ccd/CcdClientApiErrorDecoder.java
+++ src/main/java/uk/gov/hmcts/probate/services/submit/clients/v2/ccd/CcdClientApiErrorDecoder.java
@@ -4,7 +4,6 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import static feign.FeignException.errorStatus;
 
@@ -13,21 +12,28 @@ public class CcdClientApiErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        log.error("CcdClientApi responded with {} - {}", response.status(), response.body());
 
-        switch (response.status()) {
-            case 400:
-                log.error("throwing ResponseStatusException BAD_REQUEST");
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, response.reason());
-            case 404:
-                log.error("throwing ResponseStatusException NOT_FOUND");
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND,  response.reason());
-            case 409:
-                log.error("throwing ResponseStatusException CONFLICT");
-                throw new ResponseStatusException(HttpStatus.CONFLICT, response.reason());
+        HttpStatus responseStatus = getHttpStatusFromResponse(response);
+        String responseBody = response.body() == null ? "" : response.body().toString();
+
+        log.error("CcdClientApi responded with {} - {}", responseStatus.value(), responseBody);
+
+        if(responseStatus.is5xxServerError() || responseStatus.is4xxClientError()){
+            throw new CcdClientApiException(responseBody, responseStatus);
         }
 
         return errorStatus(methodKey, response);
+    }
+
+    HttpStatus getHttpStatusFromResponse(Response response){
+        HttpStatus responseStatus;
+        try{
+            responseStatus = HttpStatus.valueOf(response.status());
+        } catch (IllegalArgumentException ex) {
+            log.debug("CcdClientApi responded with unprocessable HttpStatus");
+            responseStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return responseStatus;
     }
 
 }
