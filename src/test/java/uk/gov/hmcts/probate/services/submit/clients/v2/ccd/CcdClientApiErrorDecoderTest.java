e3f2b726f32244a368c67a4d0af630c4ede6a1cc
Jake <jake.huggins@solirius.com>
2019-03-18 17:36:04 +0000

    PRO-4949: amend tests to reflect change to CcdClientApiError dto

--- src/test/java/uk/gov/hmcts/probate/services/submit/clients/v2/ccd/CcdClientApiErrorDecoderTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/submit/clients/v2/ccd/CcdClientApiErrorDecoderTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.probate.model.client.CcdClientApiError;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -64,13 +65,11 @@ public class CcdClientApiErrorDecoderTest {
                 .reason("Internal server error")
                 .request(Request.create(HttpMethod.GET.toString(), "/api", Collections.emptyMap(), null, Util.UTF_8))
                 .headers(headers)
-                .body("hello world", UTF_8)
                 .build();
 
         try {
             throw errorDecoder.decode("Service#foo()", response);
         } catch (CcdClientApiException e) {
-            assertThat(e.getMessage()).isEqualTo("hello world");
             assertThat(e.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -78,18 +77,19 @@ public class CcdClientApiErrorDecoderTest {
     @Test
     public void throwsCcdClientApiExceptionIncludesBody() throws Throwable {
 
+        String ccdClientErrorResponse = "{\"exception\":\"uk.gov.hmcts.ccd.endpoint.exceptions.ResourceNotFoundException\",\"timestamp\":\"2019-03-18T12:42:22.384\",\"status\":404,\"error\":\"Not Found\",\"message\":\"No field found\",\"path\":\"/citizens/36/jurisdictions/PROBATE/case-types/GrantOfRepresentation/cases\",\"details\":null,\"callbackErrors\":null,\"callbackWarnings\":null}";
         Response response = Response.builder()
                 .status(500)
                 .reason("Internal server error")
                 .request(Request.create(HttpMethod.POST.toString(), "/api", Collections.emptyMap(), null, Util.UTF_8))
                 .headers(headers)
-                .body("hello world", UTF_8)
+                .body(ccdClientErrorResponse, UTF_8)
                 .build();
 
         try {
             throw errorDecoder.decode("Service#foo()", response);
         } catch (CcdClientApiException e) {
-            assertThat(e.getMessage()).isEqualTo("hello world");
+            assertThat(e.getCcdClientApiError().getError()).isEqualTo("Not Found");
             assertThat(e.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -101,7 +101,6 @@ public class CcdClientApiErrorDecoderTest {
                 .reason("Bad Request")
                 .request(Request.create(HttpMethod.POST.toString(), "/api", Collections.emptyMap(), null, Util.UTF_8))
                 .headers(headers)
-                .body("hello world", UTF_8)
                 .build();
 
         CcdClientApiErrorDecoder errorDecoder = new CcdClientApiErrorDecoder();
@@ -117,7 +116,6 @@ public class CcdClientApiErrorDecoderTest {
                 .reason("Internal server error")
                 .request(Request.create(HttpMethod.POST.toString(), "/api", Collections.emptyMap(), null, Util.UTF_8))
                 .headers(headers)
-                .body("hello world", UTF_8)
                 .build();
 
         CcdClientApiErrorDecoder errorDecoder = new CcdClientApiErrorDecoder();
@@ -133,13 +131,45 @@ public class CcdClientApiErrorDecoderTest {
                 .reason("Internal server error")
                 .request(Request.create(HttpMethod.POST.toString(), "/api", Collections.emptyMap(), null, Util.UTF_8))
                 .headers(headers)
-                .body("hello world", UTF_8)
                 .build();
 
         CcdClientApiErrorDecoder errorDecoder = new CcdClientApiErrorDecoder();
         HttpStatus responseStatus = errorDecoder.getHttpStatusFromResponse(response);
 
         assertThat(responseStatus).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void mapBodyToCcdApiErrorWithValidResponseBody() {
+
+        String ccdClientErrorResponse = "{\"exception\":\"uk.gov.hmcts.ccd.endpoint.exceptions.ResourceNotFoundException\",\"timestamp\":\"2019-03-18T12:42:22.384\",\"status\":500,\"error\":\"Not Found\",\"message\":\"No field found\",\"path\":\"/citizens/36/jurisdictions/PROBATE/case-types/GrantOfRepresentation/cases\",\"details\":null,\"callbackErrors\":null,\"callbackWarnings\":null}";
+        Response response = Response.builder()
+                .status(500)
+                .reason("Internal server error")
+                .request(Request.create(HttpMethod.POST.toString(), "/api", Collections.emptyMap(), null, Util.UTF_8))
+                .headers(headers)
+                .body(ccdClientErrorResponse, UTF_8)
+                .build();
+
+        CcdClientApiErrorDecoder errorDecoder = new CcdClientApiErrorDecoder();
+        CcdClientApiError apiError = errorDecoder.mapBodyToCcdApiError(response.body());
+
+        assertThat(apiError.getStatus()).isEqualTo(500);
+    }
+
+    @Test
+    public void mapBodyToCcdApiErrorWithInvalidResponseBody() {
+        Response response = Response.builder()
+                .status(400)
+                .reason("Internal server error")
+                .request(Request.create(HttpMethod.POST.toString(), "/api", Collections.emptyMap(), null, Util.UTF_8))
+                .headers(headers)
+                .build();
+
+        CcdClientApiErrorDecoder errorDecoder = new CcdClientApiErrorDecoder();
+        CcdClientApiError apiError = errorDecoder.mapBodyToCcdApiError(response.body());
+
+        assertThat(apiError.getStatus()).isEqualTo(null);
     }
 
 }
--- src/test/java/uk/gov/hmcts/probate/services/submit/clients/v2/ccd/CcdClientApiExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/submit/clients/v2/ccd/CcdClientApiExceptionHandlerTest.java
@@ -1,42 +1,50 @@
 package uk.gov.hmcts.probate.services.submit.clients.v2.ccd;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import uk.gov.hmcts.reform.probate.model.client.CcdClientApiError;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CcdClientApiExceptionHandlerTest {
 
     private CcdClientApiExceptionHandler exceptionHandler = new CcdClientApiExceptionHandler();
+    private CcdClientApiError clientApiError;
+
+    @Before
+    private void setUp() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "{\"exception\":\"uk.gov.hmcts.ccd.endpoint.exceptions.ResourceNotFoundException\",\"timestamp\":\"2019-03-18T12:42:22.384\",\"status\":404,\"error\":\"Not Found\",\"message\":\"No field found\",\"path\":\"/citizens/36/jurisdictions/PROBATE/case-types/GrantOfRepresentation/cases\",\"details\":null,\"callbackErrors\":null,\"callbackWarnings\":null}";
+        clientApiError = mapper.readValue(json,CcdClientApiError.class);
+    }
 
     @Test
-    void handleCcdClientApiExceptionReturnsResponseStatus500() throws IOException {
-
+    void handleCcdClientApiExceptionReturnsResponseStatus500(){
         ResponseEntity responseEntity = exceptionHandler
-                .handleCcdClientApiException(new CcdClientApiException("{\"test\": \"test\"}", HttpStatus.INTERNAL_SERVER_ERROR));
+                .handleCcdClientApiException(new CcdClientApiException(clientApiError, HttpStatus.INTERNAL_SERVER_ERROR));
 
         assertThat(responseEntity.getStatusCodeValue()).isEqualTo(500);
     }
 
     @Test
-    void handleCcdClientApiExceptionReturnsResponseStatus400() throws IOException {
+    void handleCcdClientApiExceptionReturnsResponseStatus400(){
 
         ResponseEntity responseEntity = exceptionHandler
-                .handleCcdClientApiException(new CcdClientApiException("{\"test\": \"test\"}", HttpStatus.BAD_REQUEST));
+                .handleCcdClientApiException(new CcdClientApiException(clientApiError, HttpStatus.BAD_REQUEST));
 
         assertThat(responseEntity.getStatusCodeValue()).isEqualTo(400);
     }
 
     @Test
-    void handleCcdClientApiExceptionWithBlankMessage() throws IOException {
+    void handleCcdClientApiExceptionWithBlankMessage(){
 
         ResponseEntity responseEntity = exceptionHandler
-                .handleCcdClientApiException(new CcdClientApiException("", HttpStatus.BAD_REQUEST));
+                .handleCcdClientApiException(new CcdClientApiException(clientApiError, HttpStatus.BAD_REQUEST));
 
         assertThat(responseEntity.getStatusCodeValue()).isEqualTo(400);
     }
