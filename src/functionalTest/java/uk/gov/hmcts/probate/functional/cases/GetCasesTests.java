458378167dacb1b842d8d0791e3d06f922300b05
Megan <meganaliceslattery@gmail.com>
2020-08-14 16:41:31 +0100

    PRO-7870: Add more get case tests

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/cases/GetCasesTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/cases/GetCasesTests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,8 +18,8 @@ public class GetCasesTests extends IntegrationTestBase {
     @Value("${idam.username}")
     private String email;
 
-    private static final String EMAIL_PLACEHOLDER = "XXXXXXXXXX";
     private Boolean setUp = false;
+    private static final String EMAIL_PLACEHOLDER = "XXXXXXXXXX";
 
     private String caseId;
 
@@ -34,11 +35,41 @@ public class GetCasesTests extends IntegrationTestBase {
                     .body(caseData)
                     .when()
                     .post("/cases/initiate");
-
             JsonPath jsonPath = JsonPath.from(response.getBody().asString());
             caseId = jsonPath.get("caseInfo.caseId");
+
             setUp = true;
         }
+    }
+
+    @Test
+    public void getCaseByIdAsPathVariableReturns200() {
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders())
+                .queryParam("caseType",CaseType.GRANT_OF_REPRESENTATION)
+                .when()
+                .get("/cases/" + caseId)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract().jsonPath().prettify();
+    }
+
+    @Test
+    public void getCaseByIncorrectIdAsPathVariableReturns404() {
+        String randomCaseId = RandomStringUtils.randomNumeric(16).toLowerCase();
+
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders())
+                .queryParam("caseType",CaseType.GRANT_OF_REPRESENTATION)
+                .when()
+                .get("/cases/" + randomCaseId)
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .extract().jsonPath().prettify();
     }
 
     @Test
@@ -56,16 +87,90 @@ public class GetCasesTests extends IntegrationTestBase {
     }
 
     @Test
-    public void getCaseByIdReturns200() {
+    public void getCaseByIncorrectApplicantEmailReturns404() {
+        String randomEmail = RandomStringUtils.randomAlphanumeric(5).toLowerCase() + "@email.com";
+
         RestAssured.given()
                 .relaxedHTTPSValidation()
                 .headers(utils.getHeaders())
                 .queryParam("caseType",CaseType.GRANT_OF_REPRESENTATION)
                 .when()
-                .get("/cases/" + caseId)
+                .get("/cases/applicantEmail/" + randomEmail)
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .extract().jsonPath().prettify();
+    }
+
+    @Test
+    public void getAllGOPCasesReturns200() {
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders())
+                .queryParam("caseType",CaseType.GRANT_OF_REPRESENTATION)
+                .when()
+                .get("/cases/all")
                 .then()
                 .assertThat()
                 .statusCode(200)
+                .extract().jsonPath().prettify();
+    }
+
+    @Test
+    public void getCaseByInviteIdReturns200 () {
+//        RestAssured.given()
+//                .relaxedHTTPSValidation()
+//                .headers(utils.getHeaders())
+//                .queryParam("caseType",CaseType.GRANT_OF_REPRESENTATION)
+//                .when()
+//                .get("/cases/invitation/" + inviteId)
+//                .then()
+//                .assertThat()
+//                .statusCode(200)
+//                .extract().jsonPath().prettify();
+    }
+
+    @Test
+    public void getCaseByIncorrectInviteIdReturns404 () {
+//        RestAssured.given()
+//                .relaxedHTTPSValidation()
+//                .headers(utils.getHeaders())
+//                .queryParam("caseType",CaseType.GRANT_OF_REPRESENTATION)
+//                .when()
+//                .get("/cases/invitation/" + inviteId)
+//                .then()
+//                .assertThat()
+//                .statusCode(404)
+//                .extract().jsonPath().prettify();
+    }
+
+    @Test
+    public void getCaseByCaseIdAsRequestParamReturns200() {
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders())
+                .queryParam("caseId", caseId)
+                .when()
+                .get("/cases")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract().jsonPath().prettify();
+    }
+
+    @Test
+    public void getCaseByIncorrectIdAsRequestParamReturns400() {
+        String randomCaseId = RandomStringUtils.randomNumeric(16).toLowerCase();
+
+        RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(utils.getHeaders())
+                .queryParam("caseId", randomCaseId)
+                .when()
+                .get("/cases")
+                .then()
+                .assertThat()
+                .statusCode(400)
                 .extract().jsonPath().prettify();
     }
 }
